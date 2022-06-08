0522bf2b3ac2aee5a7c35a4296a8c005b3535dc6
Samuel Shaw <samuelshaw2000@gmail.com>
2022-06-07 20:23:21 -0700

    "Unbroke" GameServices

--- Wordle.Api/Program.cs
+++ Wordle.Api/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
+using System.Text.Json.Serialization;
 using Wordle.Api.Data;
 using Wordle.Api.Identity;
 using Wordle.Api.Services;
